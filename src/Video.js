574e2b223ee75d8ac23d05196ed54e1378b1de56
Samitti <samuelteweldebrhan@gmail.com>
2021-12-31 19:52:24 +0300

    add video footer

--- src/Video.js
+++ src/Video.js
@@ -25,7 +25,7 @@ function Video() {
                 className='video__player'
                 loop
                 ref={videoRef}
-                src="https://v16-webapp.tiktok.com/f1ee8fc5cdb53f6a8034c293dbd07260/61c728ac/video/tos/alisg/tos-alisg-pve-0037c001/fc35be16d3294f29838007a491b7fef5/?a=1988&br=2032&bt=1016&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FGWkag3-I&l=2021122508201201018907721008A63943&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anVzb2k6ZjNqOTMzODczNEApZDhnZTY5Ozs8N2YzZTdmOmdkaDItcjQwYXNgLS1kMS1zc2NgYDBjMTQ2YzYuNF5fLl46Yw%3D%3D&vl=&vr="
+                src="https://v16-webapp.tiktok.com/a5868b60550f42a1b7d11e5072d73a80/61cf82bb/video/tos/alisg/tos-alisg-pve-0037c001/38b2068a46b849fcb93aa013b38c199f/?a=1988&br=6840&bt=3420&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FGWkag3-I&l=20211231162232010192159238188F7C63&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajZ0bzc6Zmw3OjMzODczNEApZGQ5NzloNDw6N2lnMzM4OGdjYzNlcjQwMTNgLS1kMS1zcy02NjUwYjIwYjFjNV5eMmI6Yw%3D%3D&vl=&vr="
             > </video>
             
             <VideoFooter />
